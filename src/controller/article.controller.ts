b8a1cdd82eadd37e8d18194c93bdf87e2798264e
sakibsarkar <sakibsarkar707@gmail.com>
2024-06-08 13:31:57 +0600

    query implemented

--- src/controller/article.controller.ts
+++ src/controller/article.controller.ts
@@ -1,3 +1,4 @@
+import AggregationBuilder from "../builder/QueryBuilderAgreegation";
 import { Article } from "../model/article.model";
 import { Category } from "../model/category.model";
 import { Tags } from "../model/tags.model";
@@ -23,8 +24,51 @@ export const createArticle = catchAsyncError(async (req, res) => {
 
   sendResponse(res, {
     statusCode: 200,
-    data: article,
+    data: { ...article.toObject(), categroy: categories, tags: tag },
     message: "article created successfully",
     success: true,
+  });
+});
+
+export const getAllArticle = catchAsyncError(async (req, res, next) => {
+  const { searchTerm, category, tag, limit, skip } = req.query;
+
+  const builder = new AggregationBuilder(Article.aggregate(), req.query);
+  builder.lookup("tags", "_id", "article", "tags");
+  builder.lookup("categories", "_id", "article", "category");
+  if (limit) {
+    builder.limit(parseInt(limit as string));
+  }
+  if (skip) {
+    builder.skip(parseInt(limit as string));
+  }
+  if (tag) {
+    builder.aggregation.match({
+      tags: {
+        $elemMatch: {
+          name: tag,
+        },
+      },
+    });
+  }
+  if (category) {
+    builder.aggregation.match({
+      category: {
+        $elemMatch: {
+          name: category,
+        },
+      },
+    });
+  }
+
+  if (searchTerm) {
+    builder.match(["title", "text"]);
+  }
+  const result = await builder.aggregation;
+  sendResponse(res, {
+    data: result,
+    message: "successfully get all article ",
+    success: true,
+    statusCode: 200,
   });
 });
--- src/routes/article.route.ts
+++ src/routes/article.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { createArticle } from "../controller/article.controller";
+import { createArticle, getAllArticle } from "../controller/article.controller";
 import { validSchema } from "../middleweres/validator";
 import articleSchema from "../validation/article.validation";
 const router = Router();
 router.post("/create", validSchema(articleSchema), createArticle);
+router.get("/get/all", getAllArticle);
 export const arcticleRoute = router;
